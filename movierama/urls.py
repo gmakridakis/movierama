b1cbcbe00d1768769d81bfc06a3ce7bb9bd98532
George Makridakis <george.makridakis@orfium.com>
2022-03-11 18:14:12 +0200

    Changing homepage url

--- movierama/urls.py
+++ movierama/urls.py
@@ -1,8 +1,10 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
+import movies
 
 urlpatterns = [
     path("movies", include("movies.urls")),
     path("admin/", admin.site.urls),
+    path("", movies.views.index, name="index"),
 ]
--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
+
 from movies import views
 
-urlpatterns = [
-    path("", views.index, name="index"),
-]
+urlpatterns = []
