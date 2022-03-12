0424cec842e471aa64e5316e2c0c0529127c4738
George Makridakis <george.makridakis@orfium.com>
2022-03-13 01:28:30 +0200

    Retract vote

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("add_movie", views.add_movie, name="add_movie"),
     path("user_movies/<int:user_id>", views.user_movies, name="user_movies"),
     path("add_vote", views.add_vote, name="add_vote"),
+    path("retract_vote", views.retract_vote, name="retract_vote"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -49,8 +49,8 @@ def add_vote(request):
         try:
             movie = Movie.objects.get(id=movie_id)
         except Movie.DoesNotExist:
-            print(f"User with ID {user_id} has already voted for movie with ID {movie_id}")
-            # messages.error(request, "You have already voted for this movie")
+            print(f"There is no movie with ID {movie_id}")
+            # messages.error(request, "Cannot find the requested movie")
             return redirect(request.META["HTTP_REFERER"])
 
         try:
@@ -64,6 +64,35 @@ def add_vote(request):
         except IntegrityError:
             print(f"User with ID {user_id} has already voted for movie with ID {movie_id}")
             # messages.error(request, "You have already voted for this movie")
+            return redirect(request.META["HTTP_REFERER"])
+
+    return redirect(request.META["HTTP_REFERER"])
+
+
+def retract_vote(request):
+    if request.method == "POST":
+        user_id = request.POST["user_id"]
+        movie_id = request.POST["movie_id"]
+        is_upvote = request.POST["is_upvote"]
+
+        try:
+            movie = Movie.objects.get(id=movie_id)
+        except Movie.DoesNotExist:
+            print(f"There is no movie with ID {movie_id}")
+            # messages.error(request, "Cannot find the requested movie")
+            return redirect(request.META["HTTP_REFERER"])
+
+        try:
+            vote = Vote.objects.get(user_id=user_id, movie_id=movie_id, is_upvote=is_upvote)
+            if is_upvote == "True":
+                movie.upvotes -= 1
+            else:
+                movie.downvotes -= 1
+            vote.delete()
+            movie.save()
+        except Vote.DoesNotExist:
+            print(f"User with ID {user_id} has not voted for movie with ID {movie_id}")
+            # messages.error(request, "You have not voted for this movie")
             return redirect(request.META["HTTP_REFERER"])
 
     return redirect(request.META["HTTP_REFERER"])
