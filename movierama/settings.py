fa65f2e2c8fd74647686802736d0dc30435a9da2
George Makridakis <george.makridakis@orfium.com>
2022-03-13 22:10:30 +0200

    Local settings for production server

--- movierama/settings.py
+++ movierama/settings.py
@@ -126,3 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MESSAGE_TAGS = {messages.ERROR: "danger"}
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
